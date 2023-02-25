c1bacf42409cbe5adda468a63a642eda30e11506
LorgioRoda <lorgiorodaroca@gmail.com>
2023-02-25 10:52:53 +0100

    feat: :sparkles: add spa route

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const NAVIGATION_EVENT = 'pushstate';
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const navigate = (href: string) => {
+	window.history.pushState({}, '', href); //cambia la url pero no refresca la pagina
+	console.log('href', href);
 
-export default App
+	const navigationEvent = new Event(NAVIGATION_EVENT);
+	window.dispatchEvent(navigationEvent);
+};
+
+const HomePage = () => {
+	return (
+		<div>
+			<h3>Home</h3>
+			<button onClick={() => navigate('/about')}>Here go to about</button>
+		</div>
+	);
+};
+
+const About = () => {
+	return (
+		<div>
+			<h4>About</h4>
+			<button onClick={() => navigate('/')}>Here go to HOME</button>
+		</div>
+	);
+};
+
+export const App = () => {
+	const [currentpath, setCurrentpath] = useState(window.location.pathname);
+
+	useEffect(() => {
+		const onLocationChange = () => {
+			setCurrentpath(window.location.pathname);
+		};
+
+		window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+		window.addEventListener('popstate', onLocationChange);
+
+		return () => {
+			window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+			window.removeEventListener('popstate', onLocationChange);
+		};
+	}, []);
+
+	return (
+		<>
+			{currentpath === '/' && <HomePage />}
+			{currentpath === '/about' && <About />}
+		</>
+	);
+};
